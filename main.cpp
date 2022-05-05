63b216b6c8f14b7441a1256c620b3ccb5981b36e
Daniel Siutkowski <dan.siutkowski@gmail.com>
2022-05-05 20:02:19 +0200

    added SDL_ttf and SDL_image libraries, also added images and minor changes to code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+void close();
+void fillScreenSurfrace();
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
 int main( int argc, char* args[] )
 {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
 
     
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -25,17 +31,25 @@ int main( int argc, char* args[] )
         }
         else
         {
-            //get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
+            fillScreenSurfrace();
+            SDL_Delay(2000); //delay to make window visible before program closes
 
         }
     }
-    //destroy window
-    SDL_DestroyWindow( window );
-
-    //quit SDL subsystems
-    SDL_Quit();
+    close();
 
     return 0;
+}
+
+
+void close(){
+    
+    SDL_DestroyWindow(window); //destroy window
+    SDL_Quit(); //quit SDL subsystems
+}
+
+void fillScreenSurfrace(){
+    screenSurface = SDL_GetWindowSurface( window ); //get window surface
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 255, 255)); //fill surface with color
+    SDL_UpdateWindowSurface(window); //update color changes
 }
