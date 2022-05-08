14b9696a27fe738741af7c094ba0b6a6f1193c47
Daniel Siutkowski <dan.siutkowski@gmail.com>
2022-05-08 14:55:22 +0200

    added missing libraries, created game loop, still some bugs

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdio.h>
+#define IMG_PATH "images/iks.png"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -14,28 +15,53 @@ SDL_Surface* screenSurface = NULL;
 
 int main( int argc, char* args[] )
 {
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *img = NULL;
+    int w, h;
 
-    
+    //initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        return 1;
     }
 
-    else
+    //create window
+    window = SDL_CreateWindow( "TicTacToe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL )
     {
-        //create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Can't create window! SDL_Error: %s\n", SDL_GetError() );
-        }
-        else
-        {
-            fillScreenSurfrace();
-            SDL_Delay(2000); //delay to make window visible before program closes
-
-        }
+        printf( "Can't create window! SDL_Error: %s\n", SDL_GetError() );
+        return 1;
     }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); //create accelerated renderer
+
+    img = IMG_LoadTexture(renderer, IMG_PATH);
+    SDL_QueryTexture(img, NULL, NULL, &w, &h); //get width and height from image
+    SDL_Rect texr; texr.x = SCREEN_WIDTH/2; texr.y = SCREEN_HEIGHT/2; texr.w = w; texr.h = h;
+
+    while(1){
+        SDL_Event e;
+        if ( SDL_PollEvent(&e) ) {
+            if (e.type == SDL_QUIT)
+                break;
+            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
+                break;
+        } 
+        
+        // clear the screen
+        SDL_RenderClear(renderer);
+        fillScreenSurfrace();
+        // copy the texture to the rendering context
+        SDL_RenderCopy(renderer, img, NULL, &texr);
+        // printf("Err message from img renderer: %s\n", SDL_GetError());
+        // flip the backbuffer
+        // this means that everything that we prepared behind the screens is actually shown
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(img);
+    SDL_DestroyRenderer(renderer);
     close();
 
     return 0;
